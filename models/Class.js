0719d372bd1a9d35a83c56ec31f34ebdd14e1f41
crvwler <aaababuji@gmail.com>
2025-04-11 17:13:45 +0100

    Updated the schema

--- models/Class.js
+++ models/Class.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
 
 const classSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  subject: { type: String, required: true },
+  topic: { type: String, required: true },
   date: { type: Date, required: true },
-  tasksDue: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+  time: { type: String, required: true },
+  hasTask: { type: Boolean, required: true },
 });
 
 export default mongoose.model("Class", classSchema);
--- models/Exam.js
+++ models/Exam.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const examSchema = new mongoose.Schema({
   subject: { type: String, required: true },
+  type: { type: String, required: true },
   date: { type: Date, required: true },
+  time: { type: String, required: true },
+  venue: { type: String, required: true },
 });
 
 export default mongoose.model("Exam", examSchema);
